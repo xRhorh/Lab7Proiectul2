ec2db35eeb1006c01d9863eb07f1ba5113441a18
Rares Pintilii <blackknife000@gmail.com>
2020-04-01 17:56:53 +0300

    Inceputul proiectului 2

---
+++ ObjectWCF/InterfaceWCF.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+using APIandModelLibrary;
+
+namespace ObjectWCF
+{
+    [ServiceContract]
+    interface InterfaceMyPhotosAPI
+    {
+        [OperationContract]
+        void AddPhoto(string path);
+
+        [OperationContract]
+        void AddProperty(int key_image, string title, string description);
+
+        [OperationContract]
+        void DeletePhoto(int key_image);
+
+        [OperationContract]
+        void DeleteProperty(int key_property);
+
+        [OperationContract]
+        List<FileReprezentation> GetPhotoNames();
+
+        [OperationContract]
+        List<PropertyReprezentation> GetPropertyForFile(int id_file);
+
+        [OperationContract]
+        void OpenImage(int key_image);
+
+        [OperationContract]
+        bool FileIsAvailable(int key_image);
+
+        [OperationContract]
+        List<FileReprezentation> SearchFilesByDescription(string title, string description);
+
+        [OperationContract]
+        List<FileReprezentation> SearchFilesByType(string title);
+    }
+}
